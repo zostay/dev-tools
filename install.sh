92343d622915ae025bffe9490f257fa771a2be9e
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-08-08 16:18:57 -0500

    more cleanup for installer

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/zsh
 
-git clone git@github.com:zostay/dev-tools.git $HOME/.zx
+if [[ -d "$HOME/.zx" ]]; then
+    git --git-dir="$HOME/.zx/.git" pull
+else
+    git clone git@github.com:zostay/dev-tools.git $HOME/.zx
+fi
 
 cd ~/.zx
 
-go build -o ~/.zx/bin/zxconfig ./cmd/zxconfig/...
+go build -o ~/.zx/bin ./cmd/zxconfig/...
 ./bin/zxinstall
