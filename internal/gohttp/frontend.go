7697d48dfd64ec2ae9c082e3caf8c456b0b744e4
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-08-12 22:51:32 -0500

    fixed proxy, better gohttp logging

--- internal/gohttp/frontend.go
+++ internal/gohttp/frontend.go
@@ -11,10 +11,15 @@ import (
 	"sync"
 )
 
+type proxyURL struct {
+	to    *url.URL
+	proxy *httputil.ReverseProxy
+}
+
 type Frontend struct {
 	addrs   chan net.Addr
 	plock   *sync.RWMutex
-	proxies map[string]*httputil.ReverseProxy
+	proxies map[string]proxyURL
 	s       *http.Server
 	done    *sync.WaitGroup
 	logger  *log.Logger
@@ -24,7 +29,7 @@ func New(done *sync.WaitGroup, logger *log.Logger) *Frontend {
 	return &Frontend{
 		addrs:   make(chan net.Addr),
 		plock:   new(sync.RWMutex),
-		proxies: make(map[string]*httputil.ReverseProxy),
+		proxies: make(map[string]proxyURL),
 		s:       new(http.Server),
 		done:    done,
 		logger:  logger,
@@ -34,7 +39,10 @@ func New(done *sync.WaitGroup, logger *log.Logger) *Frontend {
 func (f *Frontend) SetProxy(path string, to *url.URL) {
 	f.plock.Lock()
 	defer f.plock.Unlock()
-	f.proxies[path] = httputil.NewSingleHostReverseProxy(to)
+	f.proxies[path] = proxyURL{
+		to:    to,
+		proxy: httputil.NewSingleHostReverseProxy(to),
+	}
 }
 
 func (f *Frontend) Serve(l net.Listener) error {
@@ -59,34 +67,67 @@ func (f *Frontend) Quit() {
 	}
 }
 
+type responseWriter struct {
+	http.ResponseWriter
+	status       int
+	bytesWritten int
+	wroteHeader  bool
+}
+
+func (rw *responseWriter) Header() http.Header {
+	return rw.ResponseWriter.Header()
+}
+
+func (rw *responseWriter) Write(bs []byte) (int, error) {
+	bw, err := rw.ResponseWriter.Write(bs)
+	rw.bytesWritten = rw.bytesWritten + bw
+	return bw, err
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.wroteHeader = true
+	rw.status = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
 func (f *Frontend) MakeHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		ww := &responseWriter{ResponseWriter: w}
+
 		path := r.URL.Path
 		proxy := f.FindBestProxy(path)
 
 		if proxy == nil {
+			f.logger.Printf("%s %s [404] (- bytes) -> {nil}", r.Method, r.URL.String())
 			http.NotFoundHandler().ServeHTTP(w, r)
 			return
 		}
 
-		proxy.ServeHTTP(w, r)
+		proxy.proxy.ServeHTTP(ww, r)
+
+		if ww.wroteHeader {
+			f.logger.Printf("%s %s [%d] (%d bytes) -> %s", r.Method, r.URL.String(), ww.status, ww.bytesWritten, proxy.to)
+		} else {
+			f.logger.Printf("%s %s [200] (%d bytes) -> %s", r.Method, r.URL.String(), ww.bytesWritten, proxy.to)
+		}
 	}
 }
 
-func (f *Frontend) FindBestProxy(path string) *httputil.ReverseProxy {
+func (f *Frontend) FindBestProxy(path string) *proxyURL {
 	pp := strings.Split(path, "/")
 
 	var bestlen = 0
-	var bestproxy *httputil.ReverseProxy
+	var bestproxy *proxyURL
 
 	f.plock.RLock()
 	defer f.plock.RUnlock()
 	for trypath, proxy := range f.proxies {
-		for i := len(pp); i > 0; i-- {
-			if strings.Join(pp[:i], "/") == trypath {
+		for i := len(pp); i > strings.Count(trypath, "/")-1; i-- {
+			ppmatch := strings.Join(pp[:i], "/") + "/"
+			if ppmatch == trypath {
 				if bestlen < i {
 					bestlen = i
-					bestproxy = proxy
+					bestproxy = &proxy
 				}
 				break
 			}
