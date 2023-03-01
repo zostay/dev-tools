1f89ff9668e08d872f0d97c36cca3844e144453d
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-02-28 23:21:42 -0600

    add a kludge to make local debugging work

--- zxpm/plugin/metal/load.go
+++ zxpm/plugin/metal/load.go
@@ -9,35 +9,98 @@ import (
 
 	"github.com/zostay/dev-tools/zxpm/config"
 	"github.com/zostay/dev-tools/zxpm/plugin"
+	"github.com/zostay/dev-tools/zxpm/plugin-changelog/changelogImpl"
 )
+
+// TODO This is a cheap debugging, but should be made nicer and configgable somehow for debugging those sticky problems.
+var runPluginServerLocally = map[string]plugin.Interface{}
 
 type Clients map[string]*goPlugin.Client
 
 const devModePluginPrefix = "go run "
+
+func LoadLocalPlugin(
+	iface plugin.Interface,
+) (*goPlugin.Client, error) {
+	reattach := make(chan *goPlugin.ReattachConfig)
+	go func() {
+		goPlugin.Serve(&goPlugin.ServeConfig{
+			Test: &goPlugin.ServeTestConfig{
+				ReattachConfigCh: reattach,
+			},
+			HandshakeConfig: Handshake,
+			Plugins: goPlugin.PluginSet{
+				"task-interface": NewPlugin(iface),
+			},
+			GRPCServer: goPlugin.DefaultGRPCServer,
+		})
+	}()
+
+	rc := <-reattach
+
+	client := goPlugin.NewClient(&goPlugin.ClientConfig{
+		HandshakeConfig: Handshake,
+		Plugins: map[string]goPlugin.Plugin{
+			"task-interface": &InterfaceGRPCPlugin{},
+		},
+		Reattach:         rc,
+		AllowedProtocols: []goPlugin.Protocol{goPlugin.ProtocolGRPC},
+		// TODO Implement SecureConfig
+	})
+
+	return client, nil
+}
+
+func LoadDevModePlugin(
+	cfg *config.Config,
+	pcfg *config.PluginConfig,
+) (*goPlugin.Client, error) {
+	if !cfg.Properties.GetBool("DEV_MODE") {
+		return nil, fmt.Errorf("plugin configuration has plugins in development, but DEV_MODE is not set to true")
+	}
+
+	cmd := []string{"go", "run", pcfg.Command[len(devModePluginPrefix):]}
+
+	return NewGoPluginClient(cmd), nil
+}
+
+func NewGoPluginClient(
+	cmd []string,
+) *goPlugin.Client {
+	client := goPlugin.NewClient(&goPlugin.ClientConfig{
+		HandshakeConfig: Handshake,
+		Plugins: map[string]goPlugin.Plugin{
+			"task-interface": &InterfaceGRPCPlugin{},
+		},
+		Cmd:              exec.Command(cmd[0], cmd[1:]...), //nolint:gosec // foot guns have been handed to user, so tainted value here is expected
+		AllowedProtocols: []goPlugin.Protocol{goPlugin.ProtocolGRPC},
+		// TODO Implement SecureConfig
+	})
+	return client
+}
 
 func LoadPlugins(cfg *config.Config) (Clients, error) {
 	clients := make(Clients, len(cfg.Plugins))
 	for i := range cfg.Plugins {
 		pcfg := &cfg.Plugins[i]
 
-		cmd := []string{"sh", "-c", pcfg.Command}
-		if strings.HasPrefix(pcfg.Command, devModePluginPrefix) {
-			if !cfg.Properties.GetBool("DEV_MODE") {
-				return nil, fmt.Errorf("plugin configuration has plugins in development, but DEV_MODE is not set to true")
+		var client *goPlugin.Client
+		if plugin, wantsLocal := runPluginServerLocally[pcfg.Name]; wantsLocal {
+			var err error
+			client, err = LoadLocalPlugin(plugin)
+			if err != nil {
+				return nil, err
 			}
-
-			cmd = []string{"go", "run", pcfg.Command[len(devModePluginPrefix):]}
+		} else if strings.HasPrefix(pcfg.Command, devModePluginPrefix) {
+			var err error
+			client, err = LoadDevModePlugin(cfg, pcfg)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			cmd := []string{"sh", "-c", pcfg.Command}
+			client = NewGoPluginClient(cmd)
 		}
-
-		client := goPlugin.NewClient(&goPlugin.ClientConfig{
-			HandshakeConfig: Handshake,
-			Plugins: map[string]goPlugin.Plugin{
-				"task-interface": &InterfaceGRPCPlugin{},
-			},
-			Cmd:              exec.Command(cmd[0], cmd[1:]...), //nolint:gosec // foot guns have been handed to user, so tainted value here is expected
-			AllowedProtocols: []goPlugin.Protocol{goPlugin.ProtocolGRPC},
-			// TODO Implement SecureConfig
-		})
 
 		clients[pcfg.Name] = client
 	}
